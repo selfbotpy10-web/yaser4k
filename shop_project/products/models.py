64ab1d09bd461cc4e3f1f8ec7e0b04223b7bcc71
selfbotpy10-web <self.bot.py10@gmail.com>
2026-05-18 21:55:45 +0100

    Update models.py

--- shop_project/products/models.py
+++ shop_project/products/models.py
@@ -1,10 +1,33 @@
 from django.db import models
 
+class Category(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+
     name = models.CharField(max_length=200)
+
     price = models.IntegerField()
+
     image = models.ImageField(upload_to='products/')
+
     description = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    is_available = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
