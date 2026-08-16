d1a13c692e883cef7e6c9ad9a0ad84189c500d6d
selfbotpy10-web <self.bot.py10@gmail.com>
2026-08-17 03:17:32 +0430

    Delete shop_project directory

--- shop_project/products/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-
-def home(request):
-    products = Product.objects.all()
-    return render(request, 'index.html', {'products': products})
